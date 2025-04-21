2aca856f995ad0257c81e4acdb5c1b118aff76be
TEMPP1901 <temppol1901@gmail.com>
2025-04-21 22:37:43 +0700

    okaoka

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/planttt-store">
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route
